b95b0f429e25683aea491308588238be12929f83
pimpollo <20221085@uthh.edu.mx>
2024-12-17 16:39:00 -0600

    actualizacion de fecha

--- src/app/config/config.jsx
+++ src/app/config/config.jsx
@@ -1,5 +1,5 @@
 export const CONFIGURACIONES={
-    BASEURL:'https://surprise1-2-back.vercel.app/api',
-    BASEURL2:'http://localhost:4000/api',
+    BASEURL2:'https://surprise1-2-back.vercel.app/api',
+    BASEURL:'http://localhost:4000/api',
     
 }
