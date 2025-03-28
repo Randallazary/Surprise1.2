fbbe18d6a66b65d9fcbbf8bc4bcc2f2303b6bb62
pimpollo <20221085@uthh.edu.mx>
2025-03-27 23:46:35 -0600

    a

--- src/app/config/config.jsx
+++ src/app/config/config.jsx
@@ -1,6 +1,6 @@
 export const CONFIGURACIONES={
     BASEURL:'https://back-steel-iota.vercel.app/api',
-    BASEURL:'https://surprise1-2-back.vercel.app/api',
-    BASEURL2:'http://localhost:4000/api',
+    BASEURL2:'https://surprise1-2-back.vercel.app/api',
+    BASEURL:'http://localhost:4000/api',
     
 }
