053004327dedc7aef19e91775318213713191c95
Randallazary <randyrubio06@gmail.com>
2025-08-19 04:44:23 -0600

    base de datos

--- src/app/config/config.jsx
+++ src/app/config/config.jsx
@@ -1,6 +1,6 @@
 export const CONFIGURACIONES={
-    BASEURL2:'https://back-steel-iota.vercel.app/api',
-    BASEURL:'https://surprise1-2back.onrender.com/api',
+    BASEURL2:'https://surprise1-2.vercel.app/api',
+   
     BASEURL:'http://localhost:4000/api',
 
     
